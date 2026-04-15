1b082a4af8d5b3736a23d04421fba2179c33fa19
Moise iyaba <moiseiyaba7@gmail.com>
2026-04-15 12:00:21 +0300

    improved

--- GridLayouts/src/main/java/com/gui/layouts/Main.java
+++ GridLayouts/src/main/java/com/gui/layouts/Main.java
@@ -1,3 +1,5 @@
+package com.gui.layouts;
+
 public class Main {
     public static void main(String[] args) {
         new WorkingWithGridLayout();
---
+++ GridLayouts/src/test/java/com/gui/layouts/WorkingWithGridLayoutTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import java.awt.*;
+import javax.swing.*;
+
+public class WorkingWithGridLayoutTest {
+
+    @Test
+    public void testLayoutAndColors() {
+        WorkingWithGridLayout app = new WorkingWithGridLayout();
+        JFrame frame = app.createJFrame();
+
+        assertTrue(frame.getLayout() instanceof GridLayout, "Layout should be GridLayout");
+
+        assertEquals(Color.BLUE, app.createPanelOne().getBackground());
+        
+        assertEquals(Color.GREEN, app.createPanelTwo().getBackground());
+        
+        assertEquals(Color.WHITE, app.createPanelThree().getBackground());
+        
+        frame.dispose(); 
+    }
+}
